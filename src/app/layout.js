c3cefe71a2fbb961699447cabaee8ea05f294490
Denis Pozder <denispozder6@gmail.comm>
2024-03-08 15:36:00 +0100

    Saving Changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
